b515c36bc2fc8587cf0f25c5f6dd5f16d8b31ca6
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-01-28 08:34:11 +0545

    external domain images support config on next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*", // To allow images from any domain
+      },
+    ],
+  },
 };
 
 export default nextConfig;
