4eaaf75134e6ac7e5a8c9a3958eadae0b46e9cf4
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-01-28 09:12:02 +0545

    feat: ui of home page, startupcard compo with live data fetching configuration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,17 @@
 import SearchForm from "@/components/SearchForm";
-// import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-// import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-// import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { auth } from "@/auth";
 
 export default async function Home(
   // since everysingle next js page can get the url search params by  using searchParams parameter.
   {
-  searchParams,
-}: {
-  searchParams: Promise<{ query?: string }>; // searchParams is a promise and return the query string
-}) {
+    searchParams,
+  }: {
+    searchParams: Promise<{ query?: string }>; // searchParams is a promise and return the query string
+  }
+) {
   const query = (await searchParams).query; // get the query from searchParams
   const params = { search: query || null };
 
@@ -18,7 +19,22 @@ export default async function Home(
 
   // console.log(session?.id);
 
-  // const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+  // const posts = await client.fetch(STARTUPS_QUERY); // fetch data from sanity. to use this we have to `import { client } from "@/sanity/lib/client` But to fetch live data automatically we have to use "sanityFetch"
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+  // Post Data sample
+  // const posts = [
+  //   {
+  //      _createdAt: new Date("2023-08-06T06:15:32.000Z"),
+  //     views: 55,
+  //     auther: {_id: 1, name: "John Doe"},
+  //     _id: 1,
+  //     description: "THis is a description",
+  //     image: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  //     category: "Robots",
+  //     title: "Robotics",
+  //   }
+  // ]
 
   return (
     <>
@@ -36,23 +52,26 @@ export default async function Home(
         <SearchForm query={query} />
       </section>
 
-      {/* <section className="section_container">
+      <section className="section_container">
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard) => (
-              <StartupCard key={post?._id} post={post} />
-            ))
+            posts.map(
+              (
+                post: StartupTypeCard // StartupTypeCard is the type of post
+              ) => <StartupCard key={post?._id} post={post} />
+            )
           ) : (
             <p className="no-results">No startups found</p>
           )}
         </ul>
-      </section> */}
+      </section>
 
-      {/* <SanityLive /> */}
+      {/* when we fetch data from sanityFetch we have to render  "SanityLive" compo */}
+      <SanityLive />
     </>
   );
 }
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,5 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation. This means that the sanity will cache the content for 60 sec and then revalidate it every 60sec.
 })
