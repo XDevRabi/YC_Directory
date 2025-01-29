2eb509193a447e9c48f3142872aa9370083d1351
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-01-29 19:12:12 +0545

    feat: completed startup page viewer compo (with ppr)

--- components/View.tsx
+++ components/View.tsx
@@ -1,14 +1,22 @@
 import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
-
+import { writeClient } from "@/sanity/lib/write-client"; // let us to modify the data directly within our application.
+import { after } from "next/server"; // allows to schedule work to be executed after a response is finished. useful for tasks and other side effects that should not block the response.
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false }) // locally defining that we want to fetch the latest data using PPR. same thing can be done globally from \sanity\lib\client.ts
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-
+  // update/write the views of the startup data. Without this the views data will be static (which was added at the time of starup creation) and will not be updated.
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
