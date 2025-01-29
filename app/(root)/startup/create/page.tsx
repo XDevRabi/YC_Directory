996d94376d9919a30abdd976d55fce128b65b2c8
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-01-29 20:08:03 +0545

    feat: create startup page ui, added some shadcn input filed compo, markdown editor packages etc

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 const Page = async () => {
   const session = await auth();
 
-  if (!session) redirect("/");
+  if (!session) redirect("/"); // redirect to home page if user is not login (no session found)
 
   return (
     <>
