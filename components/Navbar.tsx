7d8b3933bace1b309c82e2f0f696f2f180523df4
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-01-30 09:13:31 +0545

    feat: update navbar ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { auth, signOut, signIn } from "@/auth";
-// import { BadgePlus, LogOut } from "lucide-react";
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = async () => {
   // Since its a server rendered component we can use async
@@ -22,7 +22,7 @@ const Navbar = async () => {
               {/* to navigate the user to the create startup page */}
               <Link href="/startup/create">
                 <span className="max-sm:hidden">Create</span>
-                {/* <BadgePlus className="size-6 sm:hidden" /> */}
+                <BadgePlus className="size-6 sm:hidden" />
               </Link>
 
               {/* to use signOut function we have to use await (since signOut function is a async function). and to use await we have to make the function async. Now the async await run on server side so we cannot to button onclick method (since it is client side) so we have to use form tag. in react 19 we can pass a server action to a form to auto submit the form to the server. */}
@@ -36,19 +36,19 @@ const Navbar = async () => {
               >
                 <button type="submit">
                   <span className="max-sm:hidden">Logout</span>
-                  {/* <LogOut className="size-6 sm:hidden text-red-500" /> */}
+                  <LogOut className="size-6 sm:hidden text-red-500" />
                 </button>
               </form>
 
               {/* to point to the user profile page */}
               <Link href={`/user/${session?.id}`}>
-                {/* <Avatar className="size-10">
+                <Avatar className="size-10">
                   <AvatarImage
                     src={session?.user?.image || ""}
                     alt={session?.user?.name || ""}
                   />
                   <AvatarFallback>AV</AvatarFallback>
-                </Avatar> */}
+                </Avatar>
               </Link>
             </>
           ) : (
